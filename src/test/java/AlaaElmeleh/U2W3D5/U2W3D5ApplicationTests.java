55495c85a406a7ee53f771e58a58ebf5efe6bc8b
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-11-17 10:01:20 +0100

    progetto inizializzato

---
+++ src/test/java/AlaaElmeleh/U2W3D5/U2W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package AlaaElmeleh.U2W3D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
