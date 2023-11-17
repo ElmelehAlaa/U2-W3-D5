4674f6c9c3fe369dde304b5fe7421622b781b185
ElmelehAlaa <alaa.elmeleh@outlook.it>
2023-11-17 16:58:03 +0100

    fixato get prenotazioni per utente e per evento , dato accesso solo all'organizer

--- src/main/java/AlaaElmeleh/U2W3D5/controllers/UserEventController.java
+++ src/main/java/AlaaElmeleh/U2W3D5/controllers/UserEventController.java
@@ -3,6 +3,7 @@ package AlaaElmeleh.U2W3D5.controllers;
 import AlaaElmeleh.U2W3D5.entities.UserEvent;
 import AlaaElmeleh.U2W3D5.services.UserEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ public class UserEventController {
     private UserEventService userEventService;
 
     @GetMapping("/user/{userId}")
+    @PreAuthorize("hasAuthority('ORGANIZER')")
     public List<UserEvent> getUserEventsByUserId(@PathVariable long userId) {
         return userEventService.findAllByUserId(userId);
     }
 
     @GetMapping("/event/{eventId}")
+    @PreAuthorize("hasAuthority('ORGANIZER')")
     public List<UserEvent> getUserEventsByEventId(@PathVariable long eventId) {
         return userEventService.findAllByEventId(eventId);
     }
